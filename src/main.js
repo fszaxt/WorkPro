0d0a1ec0ab60df59cba0cbcf22334c9fdd7b010b
fszaxt <love810618542@163.com>
2020-07-29 20:15:56 +0800

    1

--- src/main.js
+++ src/main.js
@@ -7,15 +7,18 @@ const matchList = [
 ]
 
 let url = window.location.href;
-let scripts = matchList.some(e => {
+let match = matchList.find(e => {
     return url.match(e.test);
 })
-scripts = scripts instanceof String ? [scripts] : scripts;
-console.log(scripts);
-scripts.forEach(addScriptElement);
 
-function addScriptElement(script){
-    let el = document.createElement("script");
-    el.url = baseUrl + script + ".js";
-    document.body.appendChild(el);
+if(match && match.scripts && (typeof match.scripts === "string" || Array.isArray(match.scripts))){
+    let arr = typeof match.scripts === "string" ? [match.scripts] : match.scripts;
+    arr.forEach(addScriptElement);
+
+    function addScriptElement(script){
+        let el = document.createElement("script");
+        el.setAttribute("src",baseUrl + script + ".js");
+        document.body.appendChild(el);
+    }
 }
+
